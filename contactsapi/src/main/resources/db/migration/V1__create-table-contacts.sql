6f14914dc9a6c23723814c8a85a7c7cc739648c5
DiegoCardoso <diegocardososacramento@gmail.com>
2024-04-18 11:11:57 -0300

    deploy: foi ajustado os scripts para as tabelas em postgress, configuração do dockerfile.

--- contactsapi/src/main/resources/db/migration/V1__create-table-contacts.sql
+++ contactsapi/src/main/resources/db/migration/V1__create-table-contacts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE contacts_tb (
-    contact_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    contact_id SERIAL PRIMARY KEY,
     contact_name VARCHAR(255) NOT NULL,
     contact_email VARCHAR(255) NOT NULL UNIQUE,
     contact_phone VARCHAR(20),
--- contactsapi/src/main/resources/db/migration/V2__create-table-address.sql
+++ contactsapi/src/main/resources/db/migration/V2__create-table-address.sql
@@ -1,5 +1,5 @@
 CREATE TABLE address_tb (
-    address_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    address_id BIGSERIAL PRIMARY KEY,
     street VARCHAR(255) NOT NULL,
     cep VARCHAR(10) NOT NULL,
     number BIGINT,
